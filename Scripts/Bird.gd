e9d5569e0371dde0f4120b7ff4204afb22c8d09c
DESKTOP-NGBNHNQ\Michał <mwarioxin@gmail.com>
2019-01-29 01:23:44 +0100

    Added swipe detector scene, to handle swipe events on objects Adjusted bird scene to use new swipe event Added fade out after swipe animation Cleaned old swipe protytype code Not tested on mobile

--- Scripts/Bird.gd
+++ Scripts/Bird.gd
@@ -1,108 +1,25 @@
-extends KinematicBody2D
+extends RigidBody2D
 
-export (int) var swipe_friction = 1
-
-
-var elapsed_time = 0
-var start_pos
-var speed = 0
-var start_time
-var direction = Vector2(0,0)
-var swipe_start
-
-var touch_pos = Vector2(0,0)
-var final_touch = Vector2(0,0)
-var thrown = false
-var lifted = false
+#force value of the swipe push in x direction
+export (float) var impulse_value_x = 200
+#force value of the swipe push in y direction
+export (float) var impulse_value_y = -15
 
 func init(pos):
 	global_position = pos
 
-onready var touched = false
-
-#BASIC SWIPE TESTING
-
-#func _input_event(viewport, event, shape_idx):
-#	if Input.is_mouse_button_pressed(BUTTON_LEFT):
-#		thrown = true
-#	else:
-#		thrown = false
-#	if event is InputEventScreenTouch:
-#		if event.is_pressed():
-#			print(event.position)
-#			start_pos = event.position
-#			start_time = elapsed_time
-#		else:
-#			direction = event.position - start_pos
-#			speed = (direction.length()) / (elapsed_time - start_time)
-#			direction = direction.normalized()
-#			print(direction, speed)
-
-#	if event is InputEventScreenDrag:
-#		if (event.position - position).length() < 30:
-#			thrown = true
-#			direction = event.speed
-#		else:
-#			thrown = false
-#			direction = Vector2()
-#
-#func _ready():
-#	# Called when the node is added to the scene for the first time.
-#	# Initialization here
-#	pass
-#
 func _process(delta):
 	pass
 
+
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
-#
-#
-#TRY DRAG WORKAROUND
 
-func _on_RockSwipe_input_event(viewport, event, shape_idx):
-	if event.is_action_pressed("ui_touch"):
-		print("touched rock")
-		touched = true
+func destroy():
+	queue_free()
 
-func _on_TouchArea_input_event(viewport, event, shape_idx):
-	if event.is_action_released("ui_touch"):
-		print("realeased rock")
-		touched = false
-		#Add fore impulse or tween
-		direction = 0
-
-	if touched:
-		if event is InputEventScreenDrag:
-			print("gets dragged")
-			print("ply:", position)
-			print("pos: ", event.position)
-			print("rel: ", event.relative)
-			print("vel: ", event.speed)
-			direction = event.speed
-
-			#Move the obstackle hardcode
-			#position = position + event.relative
-
-			#Move the obstackle with collision
-			move_and_collide(event.relative)
-
-
-
-
-#WORKING CLICK AND DRAG!!!!
-#func _unhandled_input(event):
-#	if event is InputEventMouseButton and not event.pressed:
-#		lifted = false
-#	if lifted and event is InputEventMouseMotion:
-#		position += event.relative
-#
-#func _input_event(viewport, event, shape_idx):
-#	if event is InputEventMouseButton and event.pressed:
-#		lifted = true
-
-
-
-
-
-
+func _on_SwipeDetector_swiped(direction):
+	# Handle swipe detection on object
+	print("Swiped to: ", direction)
+	apply_impulse(direction, Vector2(impulse_value_x * -direction.x, impulse_value_y))
+	$AnimationPlayer.play("Fade_out")
---
+++ Scripts/SwipeDetector.gd
@@ -0,0 +1,56 @@
+extends Node2D
+
+#Note Add this scene to object and connect signals to handle swipe events
+
+# Signals to send on swipe action/cancel swipe
+signal swiped(direction)
+signal swipe_canceled(start_position)
+
+# Detect if swipe was diagonal, diagonal swipe threshold
+export (float, 1.0, 1.5) var MAX_DIAGONAL_SLOPE = 1.3 # set manually
+#Swipe timeout, time for perform swipe
+onready var timer = $Timer
+
+var swipe_start_position = Vector2()
+
+
+#HANDLE SWIPE ON BIRD OBJECT
+func _on_TouchArea_input_event(viewport, event, shape_idx):
+	if not event is InputEventScreenTouch:
+		return
+	if event.pressed:
+		_start_detection(event.position)
+	elif not timer.is_stopped():
+		_end_detection(event.position)
+
+
+func _start_detection(position):
+	swipe_start_position = position
+	timer.start()
+	
+func _end_detection(position):
+	timer.stop()
+	var direction = (position - swipe_start_position).normalized()
+	#if swipe is diagonal
+	if abs(direction.x) + abs(direction.y) > MAX_DIAGONAL_SLOPE:
+		return #do nothing
+	
+	if abs(direction.x) > abs(direction.y): #horizontal swipe
+		emit_signal('swiped', Vector2(-sign(direction.x), 0.0))
+	else:
+		#may be not used by us
+		emit_signal('swiped', Vector2(0.0, -sign(direction.y))) #vertical swipe
+	
+
+func _on_Timer_timeout():
+	emit_signal('swipe_canceled', swipe_start_position)
+
+
+#example of connection and usage 
+#func _on_EXAMPLESCENE_input_event(viewport, event, shape_idx):
+#	if not event is InputEventScreenTouch:
+#		return
+#	if event.pressed:
+#		_start_detection(event.position)
+#	elif not timer.is_stopped():
+#		_end_detection(event.position)
